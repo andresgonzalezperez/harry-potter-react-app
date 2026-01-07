087b8292e4377df60aed772336d9c0ef2da8454c
Andres Gonzalez Perez <andresmgp97@gmail.com>
2026-01-07 16:27:06 +0000

    change APIURL in pages

--- src/pages/AddStudentPage.jsx
+++ src/pages/AddStudentPage.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { API_URL } from "../config/config";
 
 function AddStudentPage() {
   const navigate = useNavigate();
